b3184f6c29c43dea8e7cba5695ec61f69e65cecb
Noah Tye <noah.tye@cartesia.ai>
2025-01-14 17:23:36 -0800

    init

---
+++ agent.py
@@ -0,0 +1,64 @@
+import logging
+
+from dotenv import load_dotenv
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    JobProcess,
+    WorkerOptions,
+    cli,
+    llm,
+)
+from livekit.agents.pipeline import VoicePipelineAgent
+from livekit.plugins import openai, cartesia, deepgram, silero
+
+
+load_dotenv(dotenv_path=".env.local")
+logger = logging.getLogger("voice-agent")
+
+
+def prewarm(proc: JobProcess):
+    proc.userdata["vad"] = silero.VAD.load()
+
+
+async def entrypoint(ctx: JobContext):
+    initial_chat_context = llm.ChatContext().append(
+        role="system",
+        text=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation. "
+        ),
+    )
+
+    logger.info(f"connecting to room {ctx.room.name}")
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    # Wait for the first participant to connect
+    participant = await ctx.wait_for_participant()
+    logger.info(f"starting voice assistant for participant {participant.identity}")
+
+    # complete list of available plugins at https://docs.livekit.io/agents/plugins
+    agent = VoicePipelineAgent(
+        vad=ctx.proc.userdata["vad"],
+        stt=deepgram.STT(),
+        llm=openai.LLM(model="gpt-4o-mini"),
+        tts=cartesia.TTS(
+            model="sonic",
+            voice="794f9389-aac1-45b6-b726-9d9369183238",
+        ),
+        chat_ctx=initial_chat_context,
+    )
+
+    agent.start(ctx.room, participant)
+
+    # Once connected, we start by hardcoding a greeting
+    await agent.say(f"Hey {participant.identity}! How can I help you today?", allow_interruptions=True)
+
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+            prewarm_fnc=prewarm,
+        ),
+    )
