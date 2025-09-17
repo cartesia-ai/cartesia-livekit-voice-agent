7410de85e426fdb48f616df31182e701673e9b6c
Naman Talreja <naman.talreja@cartesia.ai>
2025-09-17 12:23:32 -0700

    upgrading livekit

--- agent.py
+++ agent.py
@@ -1,20 +1,80 @@
 import logging
+from collections.abc import AsyncIterable
 
 from dotenv import load_dotenv
+
+from livekit import rtc
 from livekit.agents import (
-    AutoSubscribe,
+    Agent,
+    AgentSession,
     JobContext,
     JobProcess,
+    MetricsCollectedEvent,
+    ModelSettings,
+    RoomInputOptions,
+    RoomOutputOptions,
+    RunContext,
     WorkerOptions,
     cli,
-    llm,
+    metrics,
 )
-from livekit.agents.pipeline import VoicePipelineAgent
-from livekit.plugins import openai, cartesia, deepgram, silero
+from livekit.agents.llm import function_tool
+from livekit.plugins import cartesia, openai, silero
+
+# uncomment to enable Krisp background voice/noise cancellation
+# from livekit.plugins import noise_cancellation
+
+logger = logging.getLogger("basic-agent")
+
+load_dotenv()
 
 
-load_dotenv(dotenv_path=".env.local")
-logger = logging.getLogger("voice-agent")
+class MyAgent(Agent):
+    def __init__(self) -> None:
+        super().__init__(
+            instructions="Your name is Kelly. You would interact with users via voice."
+            "with that in mind keep your responses concise and to the point."
+            "do not use emojis, asterisks, markdown, or other special characters in your responses."
+            "You are curious and friendly, and have a sense of humor."
+            "you will speak english to the user",
+        )
+
+    async def on_enter(self):
+        # when the agent is added to the session, it'll generate a reply
+        # according to its instructions
+        self.session.generate_reply()
+
+    async def tts_node(
+        self, text: AsyncIterable[str], model_settings: ModelSettings
+    ) -> AsyncIterable[rtc.AudioFrame]:
+        # Markdown and emoji filters are enabled in `Agent.tts_node`, markdown symbols
+        # and emojis will be removed from the text sent to the TTS model
+
+        # To disable these filters, customize the `tts_node` method and
+        # use `return Agent.default.tts_node(self, text, model_settings)` instead
+
+        return super().tts_node(text, model_settings)
+
+    # all functions annotated with @function_tool will be passed to the LLM when this
+    # agent is active
+    @function_tool
+    async def lookup_weather(
+        self, context: RunContext, location: str, latitude: str, longitude: str
+    ):
+        """Called when the user asks for weather related information.
+        Ensure the user's location (city or region) is provided.
+        When given a location, please estimate the latitude and longitude of the location and
+        do not ask the user for them.
+
+        Args:
+            location: The location they are asking for
+            latitude: The latitude of the location, do not ask user for it
+            longitude: The longitude of the location, do not ask user for it
+        """
+
+        logger.info(f"Looking up weather for {location}")
+
+        return "sunny with a temperature of 70 degrees."
 
 
 def prewarm(proc: JobProcess):
@@ -22,43 +82,52 @@ def prewarm(proc: JobProcess):
 
 
 async def entrypoint(ctx: JobContext):
-    initial_chat_context = llm.ChatContext().append(
-        role="system",
-        text=(
-            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
-            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation. "
-        ),
-    )
+    # each log entry will include these fields
+    ctx.log_context_fields = {
+        "room": ctx.room.name,
+    }
 
-    logger.info(f"connecting to room {ctx.room.name}")
-    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
-
-    # Wait for the first participant to connect
-    participant = await ctx.wait_for_participant()
-    logger.info(f"starting voice assistant for participant {participant.identity}")
-
-    # complete list of available plugins at https://docs.livekit.io/agents/plugins
-    agent = VoicePipelineAgent(
+    session = AgentSession(
         vad=ctx.proc.userdata["vad"],
-        stt=deepgram.STT(),
+        # any combination of STT, LLM, TTS, or realtime API can be used
         llm=openai.LLM(model="gpt-4o-mini"),
-        tts=cartesia.TTS(
-            model="sonic",
-            voice="794f9389-aac1-45b6-b726-9d9369183238",
-        ),
-        chat_ctx=initial_chat_context,
+        stt=cartesia.STT(),
+        tts=cartesia.TTS(),
+        # allow the LLM to generate a response while waiting for the end of turn
+        preemptive_generation=True,
+        # sometimes background noise could interrupt the agent session, these are considered false positive interruptions
+        # when it's detected, you may resume the agent's speech
+        resume_false_interruption=True,
+        false_interruption_timeout=1.0,
+        min_interruption_duration=0.2,  # with false interruption resume, interruption can be more sensitive
+        # use LiveKit's turn detection model
     )
 
-    agent.start(ctx.room, participant)
+    # log metrics as they are emitted, and total usage after session is over
+    usage_collector = metrics.UsageCollector()
 
-    # Once connected, we start by hardcoding a greeting
-    await agent.say(f"Hey {participant.identity}! How can I help you today?", allow_interruptions=True)
+    @session.on("metrics_collected")
+    def _on_metrics_collected(ev: MetricsCollectedEvent):
+        metrics.log_metrics(ev.metrics)
+        usage_collector.collect(ev.metrics)
+
+    async def log_usage():
+        summary = usage_collector.get_summary()
+        logger.info(f"Usage: {summary}")
+
+    # shutdown callbacks are triggered when the session is over
+    ctx.add_shutdown_callback(log_usage)
+
+    await session.start(
+        agent=MyAgent(),
+        room=ctx.room,
+        room_input_options=RoomInputOptions(
+            # uncomment to enable Krisp BVC noise cancellation
+            # noise_cancellation=noise_cancellation.BVC(),
+        ),
+        room_output_options=RoomOutputOptions(transcription_enabled=True),
+    )
 
 
 if __name__ == "__main__":
-    cli.run_app(
-        WorkerOptions(
-            entrypoint_fnc=entrypoint,
-            prewarm_fnc=prewarm,
-        ),
-    )
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, prewarm_fnc=prewarm))
